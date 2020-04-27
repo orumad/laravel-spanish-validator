c3e378e672496cce95175e97ff70d50ed27f521a
Daniel Muñoz <dev@danielmunoz.io>
2020-04-27 11:35:42 +0200

    Fix some styles

--- src/SpanishValidatorServiceProvider.php
+++ src/SpanishValidatorServiceProvider.php
@@ -2,8 +2,8 @@
 
 namespace Orumad\SpanishValidator;
 
-use Illuminate\Support\ServiceProvider;
 use Illuminate\Support\Facades\Validator;
+use Illuminate\Support\ServiceProvider;
 
 class SpanishValidatorServiceProvider extends ServiceProvider
 {
