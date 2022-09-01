dcce0217d5ddfc952067296a6440bb3f999a1cb5
Enric Vidal <enric.vidal@gmail.com>
2022-09-01 22:39:26 +0200

    Code indentation

--- src/SpanishValidatorServiceProvider.php
+++ src/SpanishValidatorServiceProvider.php
@@ -11,22 +11,19 @@ class SpanishValidatorServiceProvider extends ServiceProvider
     {
 
         // Laravel >= 9 - lang folder at /lang
-        if(file_exists(base_path('lang/')))
-        {
+        if (file_exists(base_path('lang/'))) {
             $this->publishes([
-                __DIR__.'/../resources/lang' => base_path('lang/'),
+                __DIR__ . '/../resources/lang' => base_path('lang/'),
             ]);
 
             $this->loadTranslationsFrom(base_path('lang/'), 'spanishValidator');
-        }
-        else
-        {
+        } else {
             // Laravel < 9 - lang folder at /resources/lang
             $this->publishes([
-                __DIR__.'/../resources/lang' => resource_path('lang/'),
+                __DIR__ . '/../resources/lang' => resource_path('lang/'),
             ]);
 
-            $this->loadTranslationsFrom(__DIR__.'/../resources/lang/', 'spanishValidator');
+            $this->loadTranslationsFrom(__DIR__ . '/../resources/lang/', 'spanishValidator');
         }
 
 
