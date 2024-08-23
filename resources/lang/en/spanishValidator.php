19584d49e872fb0db0be3b0da0b9cf18903411b4
Daniel Muñoz <daniel@d2pro.es>
2024-08-23 13:33:52 +0200

    fix style ci issues

--- resources/lang/en/spanishValidator.php
+++ resources/lang/en/spanishValidator.php
@@ -1,13 +1,13 @@
 <?php
 
 return [
-    'spanish_tax_number'    => 'This is not a valid spanish tax number.',
-    'spanish_personal_id'   => 'This is not a valid personal id number.',
-    'nif'                   => 'This is not a valid NIF.',
-    'nie'                   => 'This is not a valid NIE.',
-    'cif'                   => 'This is not a valid CIF.',
-    'nss'                   => 'This is not a valid NSS.',
-    'iban'              => 'This is not a valid IBAN.',
-    'spanish_postal_code'   => 'The postal code is not valid.',
-    'spanish_phone'         => 'Is not a valid spanih phone number.',
+    'spanish_tax_number' => 'This is not a valid spanish tax number.',
+    'spanish_personal_id' => 'This is not a valid personal id number.',
+    'nif' => 'This is not a valid NIF.',
+    'nie' => 'This is not a valid NIE.',
+    'cif' => 'This is not a valid CIF.',
+    'nss'=> 'This is not a valid NSS.',
+    'iban' => 'This is not a valid IBAN.',
+    'spanish_postal_code' => 'The postal code is not valid.',
+    'spanish_phone' => 'Is not a valid spanih phone number.',
 ];
